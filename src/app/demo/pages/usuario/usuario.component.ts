85c82bfe4f886d10a4c9260be9c02519f20a5fa7
gmora-asd <lmora@grupoasd.com.co>
2025-03-31 16:23:02 -0500

    feat: ajustes en el modal de crear/actualizar usuario

--- src/app/demo/pages/usuario/usuario.component.ts
+++ src/app/demo/pages/usuario/usuario.component.ts
@@ -4,19 +4,22 @@ import { CommonModule } from '@angular/common';
 import { UsuarioService } from './service/usuario.service';
 import { Usuario } from 'src/app/models/usuario';
 import Swal from 'sweetalert2';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators, FormsModule, ReactiveFormsModule, AbstractControl } from '@angular/forms';
 // Importa los objetos necesarios de Bootstrap
 declare const bootstrap: any;
 
 @Component({
   selector: 'app-usuario',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './usuario.component.html',
   styleUrl: './usuario.component.scss'
 })
 export class UsuarioComponent {
   usuarios: Usuario[] = [];
   modalInstance: any;
+  modoFormulario: string = '';
+
+  usuarioSelected: Usuario;
 
   form: FormGroup = new FormGroup({
     nombreCompleto: new FormControl(''),
@@ -40,6 +43,10 @@ export class UsuarioComponent {
     });
   }
 
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
   cargarListaUsuarios() {
     this.usuarioService.getUsuarios().subscribe({
       next: (data) => {
@@ -52,8 +59,11 @@ export class UsuarioComponent {
     });
   }
 
-  crearUsuarioModal() {
+  crearUsuarioModal(modoForm: string) {
+    this.modoFormulario = modoForm;
     const modalElement = document.getElementById('crearUsuarioModal');
+    modalElement.blur();
+    modalElement.setAttribute('aria-hidden', 'false');
     if (modalElement) {
       // Verificar si ya existe una instancia del modal
       if (!this.modalInstance) {
@@ -63,10 +73,36 @@ export class UsuarioComponent {
     }
   }
 
-  cerrarModal() {
+  cerrarModal() { 
     this.form.reset();
+    this.form.markAsPristine();
+    this.form.markAsUntouched();
+    this.form.reset({
+      nombreCompleto: "",
+      correo: "",
+      telefono: ""
+    });
     if (this.modalInstance) {
       this.modalInstance.hide();
+    }
+  }
+
+  abrirModoEdicion(usuario: Usuario) {
+    this.crearUsuarioModal('E');
+    this.usuarioSelected = usuario;
+    console.log(this.usuarioSelected);
+  }
+
+  guardarActualizarUsuario() {
+    console.log('Entro');
+    console.log(this.form.valid);
+    if (this.form.valid) {
+      console.log('El formualario es valido');
+      if (this.modoFormulario.includes('C')) {
+        console.log('Creamos un usuario nuevo');
+      } else {
+        console.log('Actualizamos un usuario existente');
+      }
     }
   }
 }
