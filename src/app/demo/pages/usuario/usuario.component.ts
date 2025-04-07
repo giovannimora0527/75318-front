bc4c90e70c810b9d7654c0b32c6b05795a145e93
gmora-asd <lmora@grupoasd.com.co>
2025-04-07 18:48:23 -0500

    ajustes para crear y actualizar usuario

--- src/app/demo/pages/usuario/usuario.component.ts
+++ src/app/demo/pages/usuario/usuario.component.ts
@@ -25,7 +25,8 @@ export class UsuarioComponent {
   form: FormGroup = new FormGroup({
     nombre: new FormControl(''),
     correo: new FormControl(''),
-    telefono: new FormControl('')
+    telefono: new FormControl(''),
+    activo: new FormControl('')
   });
 
   constructor(
@@ -40,7 +41,8 @@ export class UsuarioComponent {
     this.form = this.formBuilder.group({
       nombre: ['', [Validators.required]],
       correo: ['', [Validators.required, Validators.email]],
-      telefono: ['', [Validators.required]]
+      telefono: ['', [Validators.required]],
+      activo: ['', [Validators.required]]
     });
   }
 
@@ -62,7 +64,7 @@ export class UsuarioComponent {
   crearUsuarioModal(modoForm: string) {
     this.titleModal = modoForm == 'C' ? 'Crear Usuario' : 'Editar Usuario';
     this.modoFormulario = modoForm;
-    const modalElement = document.getElementById('crearUsuarioModal');     
+    const modalElement = document.getElementById('crearUsuarioModal');
     if (modalElement) {
       // Verificar si ya existe una instancia del modal
       if (!this.modalInstance) {
@@ -77,38 +79,48 @@ export class UsuarioComponent {
     this.form.markAsPristine();
     this.form.markAsUntouched();
     this.form.reset({
-      nombreCompleto: '',
+      nombre: '',
       correo: '',
-      telefono: ''
+      telefono: '',
+      activo: ''
     });
     if (this.modalInstance) {
       this.modalInstance.hide();
     }
+    this.usuarioSelected = null;
   }
 
   abrirModoEdicion(usuario: Usuario) {
     this.usuarioSelected = usuario;
+    this.form.patchValue({
+      nombre: this.usuarioSelected.nombre,
+      correo: this.usuarioSelected.correo,
+      telefono: this.usuarioSelected.telefono,
+      activo: !!this.usuarioSelected.activo // asegura que sea booleano
+    });
     this.crearUsuarioModal('E');
   }
 
   guardarActualizarUsuario() {
     console.log(this.form.valid);
-    if (this.form.valid) {    
-      if (this.modoFormulario.includes('C')) {       
-        this.usuarioService.guardarUsuarioNuevo(this.form.getRawValue()).subscribe(
-          {
-            next: (data) => {
-              console.log(data);
-              this.showMessage("Éxito", data.message, "success");
-              this.cargarListaUsuarios();
-              this.cerrarModal();                            
-            },
-            error: (error) => {
-              console.log(error);
-              this.showMessage("Error", error.error.message, "error");
-            }
+    if (this.modoFormulario === 'C') {
+      this.form.get('activo').setValue(true);
+    }
+    console.log(this.form.getRawValue());    
+    if (this.form.valid) {
+      if (this.modoFormulario.includes('C')) {
+        this.usuarioService.guardarUsuarioNuevo(this.form.getRawValue()).subscribe({
+          next: (data) => {
+            console.log(data);
+            this.showMessage('Éxito', data.message, 'success');
+            this.cargarListaUsuarios();
+            this.cerrarModal();
+          },
+          error: (error) => {
+            console.log(error);
+            this.showMessage('Error', error.error.message, 'error');
           }
-        );
+        });
       } else {
         const idUsuario = this.usuarioSelected.idUsuario;
         // Actualizar solo los campos específicos
@@ -117,23 +129,21 @@ export class UsuarioComponent {
           ...this.form.getRawValue() // Sobrescribir con los valores del formulario
         };
         this.usuarioSelected.idUsuario = idUsuario;
-        console.log(this.usuarioSelected);     
+        console.log(this.usuarioSelected);
         // Actualizamos el usuario
-        this.usuarioService.actualizarUsuario(this.usuarioSelected).subscribe(
-          {
-            next: (data) => {
-              this.showMessage("Éxito", data.message, "success");
-              this.cargarListaUsuarios();
-              this.cerrarModal(); 
-              console.log(data);  
-              this.usuarioSelected = null;     
-            },
-            error: (error) => {
-              console.log(error.error.message);
-              this.showMessage("Error", error.error.message, "error");
-            }
+        this.usuarioService.actualizarUsuario(this.usuarioSelected).subscribe({
+          next: (data) => {
+            this.showMessage('Éxito', data.message, 'success');
+            this.cargarListaUsuarios();
+            this.cerrarModal();
+            console.log(data);
+            this.usuarioSelected = null;
+          },
+          error: (error) => {
+            console.log(error.error.message);
+            this.showMessage('Error', error.error.message, 'error');
           }
-        );
+        });
       }
     }
   }
@@ -143,7 +153,7 @@ export class UsuarioComponent {
       title: title,
       text: text,
       icon: icon,
-      confirmButtonText: 'Aceptar',      
+      confirmButtonText: 'Aceptar',
       customClass: {
         container: 'position-fixed',
         popup: 'swal-overlay'
