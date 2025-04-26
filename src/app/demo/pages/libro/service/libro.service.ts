b970d17d32c4f22170557f07b7ed146f0a6ba03c
hcarrillo <cuentadeharrison@gmail.com>
2025-04-26 08:10:40 -0500

    Feat: Libro  Crear-Actualizar-Editar

--- src/app/demo/pages/libro/service/libro.service.ts
+++ src/app/demo/pages/libro/service/libro.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Libro } from 'src/app/models/libro';
+import { RespuestaGenerica } from 'src/app/models/respuesta-gen';
 import { BackendService } from 'src/app/services/backend.service';
 import { environment } from 'src/environments/environment';
 
@@ -13,7 +14,15 @@ export class LibroService {
   constructor(private backendService: BackendService) {}
 
   getLibros(): Observable<Libro[]> {
-    return this.backendService.get(environment.apiUrl, this.api, 'listar');
+    return this.backendService.get(environment.apiUrl, this.api, "listar")
+  }
+
+  crearLibro(libro: Libro): Observable<RespuestaGenerica> {
+    return this.backendService.post(environment.apiUrl, this.api, "crear-libro", libro); 
+  }
+
+  actualizarLibro(libro: Libro) : Observable<RespuestaGenerica> {
+    return this.backendService.post(environment.apiUrl, this.api, "actualizar-libro", libro);
   }
 
 }
